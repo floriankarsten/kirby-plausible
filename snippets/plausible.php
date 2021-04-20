836857b414b129d6d8e264760464c3362f47f76a
iskrisis <code@floriankarsten.com>
2021-04-20 17:32:06 +0200

    Update plausible.php

--- snippets/plausible.php
+++ snippets/plausible.php
@@ -1,3 +1,3 @@
-<?php if(option('debug') === false): ?>
+<?php if(option('debug') === false && !kirby()->user()): ?>
 <script async defer data-domain="<?= option('floriankarsten.plausible.domain') ?? parse_url($kirby->url('index'))['host'] ?>" src="https://plausible.io/js/plausible.js"></script>
 <?php endif; ?>
