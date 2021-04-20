c16bb201a2fbd659708a7f4abed556c05516e4fd
iskrisis <code@floriankarsten.com>
2021-04-20 17:25:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ panel.plugin("floriankarsten/plausible", {
 			icon: "road-sign",
 			component: {
 				template: `
-				<div class="k-plausible-view">
+				<k-view class="k-plausible-view">
 					<div style="margin-top: 30px; text-align: center;" v-if="showError">
 						<code>You need to set floriankarsten.plausible.sharedLink in config.php</code>
 					</div>
 					<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
-				</div>`,
+				</k-view>`,
 				data: function() {
 					return {
 						options: {},
