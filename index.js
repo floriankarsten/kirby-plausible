b1a3e2b6a94ef51c504486360e55862e07f1daeb
iskrisis <code@floriankarsten.com>
2021-11-04 17:49:55 +0100

    Update embed visuals so it fits into kirby

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ panel.plugin("floriankarsten/plausible", {
 			template: `
 				<k-inside>
 					<k-view class="k-plausible-view">
-						<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+						<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink + '&embed=true&theme=light&background=%23efefef'" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
 						<div style="margin-top: 30px; text-align: center;" v-else>
 							<code>You need to set floriankarsten.plausible.sharedLink in config.php</code>
 						</div>
 					</k-view>
 				</k-inside>
 			`,
-			props: ["sharedLink"]
+			props: ["sharedLink"],
 		},
 	},
 });
