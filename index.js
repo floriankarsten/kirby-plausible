1e8ac4bee68afa985e65783ba071728411450a6e
Thomas Günther <mail@medienbaecker.com>
2022-09-15 10:27:24 +0200

    Fix mobile iframe height

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ panel.plugin("floriankarsten/plausible", {
 			template: `
 				<k-inside>
 					<k-view class="k-plausible-view">
-						<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink + '&embed=true&theme=light&background=%23efefef'" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+						<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink + '&embed=true&theme=light&background=%23efefef'" scrolling="no" frameborder="0" loading="lazy" class="plausible-iframe"></iframe>
 						<div style="margin-top: 30px; text-align: center;" v-else>
 							<code>You need to set floriankarsten.plausible.sharedLink in config.php</code>
 						</div>
