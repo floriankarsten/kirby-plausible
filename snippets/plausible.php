dfeba4836afd2805fc8f11521b3805ce87bb2340
iskrisis <code@floriankarsten.com>
2023-02-22 15:18:00 +0100

    Fixes snippet when option('debug') was null
    https://github.com/floriankarsten/kirby-plausible/issues/5

--- snippets/plausible.php
+++ snippets/plausible.php
@@ -1,3 +1,3 @@
-<?php if(option('debug') === false && !kirby()->user()): ?>
+<?php if(option('debug') !== true && !kirby()->user()): ?>
 <script defer data-domain="<?= option('floriankarsten.plausible.domain') ?? parse_url($kirby->url('index'))['host'] ?>" src="https://plausible.io/js/plausible.js"></script>
 <?php endif; ?>
