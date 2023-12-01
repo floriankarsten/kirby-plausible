09dc79e062216e551bda1fed181fa2f751ed1a58
iskrisis <code@floriankarsten.com>
2023-12-01 14:43:49 +0100

    Kirby 4 fix

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ panel.plugin("floriankarsten/plausible", {
 	components: {
 		'k-plausible-view': {
 			template: `
-				<k-inside>
-					<k-view class="k-plausible-view">
-						<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink + '&embed=true&theme=light&background=%23efefef'" scrolling="no" frameborder="0" loading="lazy" class="plausible-iframe"></iframe>
-						<div style="margin-top: 30px; text-align: center;" v-else>
-							<code>You need to set floriankarsten.plausible.sharedLink in config.php</code>
-						</div>
-					</k-view>
-				</k-inside>
+				<k-panel-inside class="k-plausible-view">
+						<k-section>
+							<iframe v-if="sharedLink" plausible-embed v-bind:src="sharedLink + '&embed=true&theme=light&background=%23efefef'" scrolling="no" frameborder="0" loading="lazy" class="plausible-iframe"></iframe>
+							<script async src="https://plausible.io/js/embed.host.js"></script>
+							<div style="margin-top: 30px; text-align: center;" v-else>
+								<code>You need to set floriankarsten.plausible.sharedLink in config.php</code>
+							</div>
+						</k-section>
+				</k-panel-inside>
 			`,
 			props: ["sharedLink"],
 		},
