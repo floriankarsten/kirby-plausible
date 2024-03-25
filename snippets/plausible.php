53bae3545414f129be51ba5ee6bc84850350ebf5
dserv-nh <54544490+dserv-nh@users.noreply.github.com>
2024-03-25 14:14:57 +0100

    feat: support custom script host and name

--- snippets/plausible.php
+++ snippets/plausible.php
@@ -1,3 +1,3 @@
 <?php if(option('debug') !== true && !kirby()->user()): ?>
-<script defer data-domain="<?= option('floriankarsten.plausible.domain') ?? parse_url($kirby->url('index'))['host'] ?>" src="https://plausible.io/js/plausible.js"></script>
+<script defer data-domain="<?= option('floriankarsten.plausible.domain') ?? parse_url($kirby->url('index'))['host'] ?>" src="<?= option('floriankarsten.plausible.scriptHost') ?? 'https://plausible.io' ?>/js/<?= option('floriankarsten.plausible.scriptName') ?? 'plausible' ?>.js"></script>
 <?php endif; ?>
